5f5efede3432691046fbe55bf8b5397be64dbae4
Reemolke <riquelmeraj@gmail.com>
2024-10-31 13:01:50 +0100

    Errores

--- Proyecto 3/Ejercicio2.js
+++ Proyecto 3/Ejercicio2.js
@@ -7,3 +7,44 @@ const currentYear = new Date().getFullYear();
         option.textContent = year;
         select.appendChild(option);
     }
+    document.getElementById('form').addEventListener('submit', function(event) {
+        const nombre = document.getElementById('nombre');
+        const apellido1 = document.getElementById('apellido1');
+        const apellido2 = document.getElementById('apellido2');
+        const fecha = document.getElementById('fecha');
+        const estudios = document.getElementById('estudios');
+        const curso = document.getElementById('curso');
+        const telefono = document.getElementById('telefono');
+
+        event.preventDefault();
+        let errores = [];
+        
+        if (!nombre.value) {
+            errores.push("Completa el campo nombre.");
+        }
+        if (!apellido1.value) {
+            errores.push("Completa el campo apellido 1.");
+        }
+        if (!apellido2.value) {
+            errores.push("Completa el campo apellido 2.");
+        }
+        if (!fecha.value) {
+            errores.push("Completa el campo fecha de nacimiento.");
+        } 
+        if (!estudios.value) {
+            errores.push("Completa el campo estudios.");
+        }
+        if (!curso.value) {
+            errores.push("Completa el campo curso.");
+        }
+        if (!telefono.value) {
+            errores.push("Completa el campo teléfono.");
+        }else if(telefono.value.length != 9) {
+            errores.push("El teléfono debe tener 9 dígitos.");
+        }
+        
+        if (errores.length > 0) {
+            alert(errores.join("\n"));
+            return;
+        }
+    });
